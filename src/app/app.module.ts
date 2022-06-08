42096fb33a7daec8aa22615c847afd012347c9bd
ManikandanKumarkp <manikandankumarkp@gmail.com>
2022-06-08 21:30:45 +0530

    1 file modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { MatCard } from '@angular/material';
 
 
 import { AppComponent } from './app.component';
 
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent,  ],
+  imports:      [ BrowserModule, FormsModule, MatCard ],
+  declarations: [ AppComponent  ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
