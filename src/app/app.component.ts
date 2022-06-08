e7aa6f73945115413a31d97b7d6ad3792bf4b162
ManikandanKumarkp <manikandankumarkp@gmail.com>
2022-06-08 19:23:49 +0530

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component, VERSION } from '@angular/core';
+import { Component , Input } from '@angular/core';
 
 @Component({
   selector: 'my-app',
@@ -6,5 +7,5 @@ import { Component, VERSION } from '@angular/core';
   styleUrls: [ './app.component.css' ]
 })
 export class AppComponent  {
-  
+  @Input name:string = "Manikandan ";
 }
