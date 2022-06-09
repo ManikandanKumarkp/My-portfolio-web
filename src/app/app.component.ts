d343df6f01a1df2fe3c57aae9990fd290c89ba02
ManikandanKumarkp <manikandankumarkp@gmail.com>
2022-06-09 09:54:25 +0530

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,6 +7,5 @@ import { Component } from '@angular/core';
   styleUrls: [ './app.component.css' ]
 })
 export class AppComponent  {
-  Name :string = "Manikandan";
-
+Name :string = "Manikandan";
 }
